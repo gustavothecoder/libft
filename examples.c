4d56d7430f9be162e8e719688db2022459004c97
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-09-12 16:09:19 -0300

    Create a g_bzero example

--- examples.c
+++ examples.c
@@ -2,9 +2,11 @@
 #include "my_c_lib.h"
 
 void run_g_memset_example();
+void run_g_bzero_example();
 
 int main() {
     run_g_memset_example();
+    run_g_bzero_example();
     return 0;
 }
 
@@ -15,4 +17,13 @@ void run_g_memset_example() {
     g_memset(str, 'a', 3);
     printf("Function call: g_memset(str, 'a', 3);\n");
     printf("After: %s\n", str);
+}
+
+void run_g_bzero_example() {
+    printf("--- g_bzero ---\n");
+    char str[] = "hello world";
+    printf("Before: %s\n", str);
+    g_bzero(str, sizeof(str));
+    printf("Function call: g_bzero(str, sizeof(str));\n");
+    printf("After: %s (was erased)\n", str);
 }
