0e11652a0fd086820658e9f7d4303d0a1b76a2c2
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-09-12 16:06:20 -0300

    Create the assert_null function

--- tests/c_tester.h
+++ tests/c_tester.h
@@ -2,5 +2,7 @@
 #include <stddef.h>
 
 #define assert_string(string1, string2, size) compare_strings(string1, string2, size, __FILE__, __LINE__)
+#define assert_null(target) check_null(target, __FILE__, __LINE__)
 
 void compare_strings(char string1[], char string2[], size_t size, char caller[], int line);
+void check_null(void *target, char caller[], int line);
