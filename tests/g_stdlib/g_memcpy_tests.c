74ae0711c3180d749343f66a28d82c47a53625f0
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-09-26 20:08:15 -0300

    Create the g_memcpy tests

---
+++ tests/g_stdlib/g_memcpy_tests.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tests.h"
+
+static void when_trying_to_copy_elements_from_char_array();
+static void when_trying_to_copy_elements_from_int_array();
+
+void run_g_memcpy_tests() {
+    when_trying_to_copy_elements_from_char_array();
+    when_trying_to_copy_elements_from_int_array();
+}
+
+static void when_trying_to_copy_elements_from_char_array() {
+    char source[] = "once upon a midnight dreary...", dest[4];
+
+    void *result = g_memcpy(dest, source, sizeof dest);
+
+    assert_array(dest, "once", sizeof dest);
+    assert(result == dest);
+}
+
+static void when_trying_to_copy_elements_from_int_array() {
+    int sz = 3*sizeof(int);
+    int *p = malloc(sz);
+    int arr[3] = {1,2,3};
+
+    void *result = g_memcpy(p, arr, sz);
+
+    assert_array(p, arr, sz);
+    assert(result == p);
+}
