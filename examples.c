af2fb433a482adaddb0127fb759d21f8452dcb35
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-09-26 20:17:43 -0300

    Create a g_memcpy example

--- examples.c
+++ examples.c
@@ -3,10 +3,12 @@
 
 static void run_g_memset_example();
 static void run_g_bzero_example();
+static void run_g_memcpy_example();
 
 int main() {
     run_g_memset_example();
     run_g_bzero_example();
+    run_g_memcpy_example();
     return 0;
 }
 
@@ -26,4 +28,14 @@ static void run_g_bzero_example() {
     g_bzero(str, sizeof(str));
     printf("Function call: g_bzero(str, sizeof(str));\n");
     printf("After: %s (was erased)\n", str);
+}
+
+static void run_g_memcpy_example() {
+    printf("--- g_memcpy ---\n");
+    char src[] = "hello world";
+    char dest[12];
+    printf("Before: src = %s, dest = %s (nothing)\n", src, dest);
+    g_memcpy(dest, src, sizeof(src));
+    printf("Function call: g_memcpy(dest, src, sizeof(src));\n");
+    printf("After: src = %s, dest = %s\n", src, dest);
 }
