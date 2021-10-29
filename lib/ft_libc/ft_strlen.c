daca0cd44f89ea57532ee02c596aa557f935683d
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-10-28 21:41:14 -0300

    Implement the ft_strlen

---
+++ lib/ft_libc/ft_strlen.c
@@ -0,0 +1,11 @@
+#include "ft_libc.h"
+
+size_t ft_strlen(const char *str) {
+    char current_char;
+    size_t counter = 0;
+    while(true) {
+        current_char = str[counter];
+        if (current_char == '\0') return counter;
+        counter++;
+    }
+}
