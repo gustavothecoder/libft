92e1f3bf2075aa1f282b8b4b0e97e3ce94ef0831
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-11-20 20:20:49 -0300

    Implement the ft_strlcat

---
+++ lib/ft_libc/ft_strlcat.c
@@ -0,0 +1,9 @@
+#include "ft_libc.h"
+
+size_t ft_strlcat(char *dest, const char *src, size_t size) {
+    size_t dest_index = ft_strlen(dest);
+    for (; dest_index < size && *src != '\0'; dest_index++, src++)
+        dest[dest_index] = *src;
+    dest[++dest_index] = '\0';
+    return dest_index;
+}
