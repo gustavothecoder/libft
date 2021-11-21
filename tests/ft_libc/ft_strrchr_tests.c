5ccec1a5d55d9b71013e81a16f5639af28163900
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-11-21 19:25:41 -0300

    Create ft_strrchr tests

---
+++ tests/ft_libc/ft_strrchr_tests.c
@@ -0,0 +1,28 @@
+#include "tests.h"
+
+static void when_ch_is_found();
+static void when_ch_is_not_found();
+
+void run_ft_strrchr_tests() {
+    when_ch_is_found();
+    when_ch_is_not_found();
+}
+
+static void when_ch_is_found() {
+    const char *str = "Try not. Do, or do not. There is no try.";
+    char ch = 't';
+
+    char *result = ft_strrchr(str, ch);
+
+    assert(result == &str[36]);
+    assert(*result == str[36]);
+}
+
+static void when_ch_is_not_found() {
+    const char *str = "Try not. Do, or do not. There is no try.";
+    char ch = '`';
+
+    char *result = ft_strrchr(str, ch);
+
+    assert(result == NULL);
+}
