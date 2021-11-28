f256d4237d5ba4dbbd1295b9e653a1907436c4e0
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-11-28 15:56:18 -0300

    Implement test report

--- tests/c_tester.c
+++ tests/c_tester.c
@@ -1,6 +1,14 @@
 #include "c_tester.h"
 
+#define WHITE "\033[0m"
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+
+int c_tester_successful_count = 0, c_tester_failure_count = 0;
+
 static void print_result(bool assertion_result, char caller[], int line);
+static void set_color(char *color);
 
 void call_print_result(bool assertion_result, char caller[], int line) {
     print_result(assertion_result, caller, line);
@@ -14,14 +22,35 @@ void compare_arrays(const void *arr1, const void *arr2, size_t sz, char caller[]
     print_result(result, caller, line);
 }
 
+void print_report() {
+    printf("-------------------- Result --------------------\n");
+    printf("Successful: ");
+    if (c_tester_failure_count > 0) set_color(YELLOW);
+    else set_color(GREEN);
+    printf("%d\n", c_tester_successful_count);
+    set_color(WHITE);
+    printf("Failure:    ");
+    if (c_tester_failure_count > 0) set_color(RED);
+    else set_color(GREEN);
+    printf("%d\n", c_tester_failure_count);
+    set_color(WHITE);
+    printf("Total:      %d\n", c_tester_successful_count + c_tester_failure_count);
+}
+
 static void print_result(bool assertion_result, char caller[], int line) {
     if (assertion_result) {
-        printf("\033[0;32m");
+        set_color(GREEN);
         printf("Success ");
+        c_tester_successful_count++;
     } else {
-        printf("\033[0;31m");
+        set_color(RED);
         printf("Failure ");
+        c_tester_failure_count++;
     }
-    printf("\033[0m");
+    set_color(WHITE);
     printf("in %s:%i\n", caller, line);
+}
+
+static void set_color(char *color) {
+    printf(color);
 }
