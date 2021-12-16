e6ee4b583e7f7c55b2f14027b2896bb76d35e88f
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-12-16 19:46:05 -0300

    Implement the ft_atoi

--- lib/ft_libc/ft_libc.h
+++ lib/ft_libc/ft_libc.h
@@ -23,3 +23,4 @@ char *ft_strstr(const char *str, const char *substr);
 char *ft_strnstr(const char *str, const char *substr, size_t count);
 int ft_strcmp(const char *lhs, const char *rhs);
 int ft_strncmp(const char *lhs, const char *rhs, size_t count);
+int ft_atoi(const char *str);
