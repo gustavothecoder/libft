b642db3b740e6a6c92a21a17e6e960f5b09a59ae
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-12-20 15:27:19 -0300

    Implement ft_toupper

--- lib/ft_libc/ft_libc.h
+++ lib/ft_libc/ft_libc.h
@@ -34,3 +34,4 @@ int ft_isdigit(int ch);
 int ft_isalnum(int ch);
 int ft_isascii(int ch);
 int ft_isprint(int ch);
+int ft_toupper(int ch);
---
+++ lib/ft_libc/ft_toupper.c
@@ -0,0 +1,8 @@
+#include "ft_libc.h"
+
+int ft_toupper(int ch) {
+    int ch_position = 0;
+    for (ch_position; LOWERCASE_ALPHABET[ch_position] != '\0'; ch_position++)
+        if (LOWERCASE_ALPHABET[ch_position] == ch) return UPPERCASE_ALPHABET[ch_position];
+    return ch;
+}
