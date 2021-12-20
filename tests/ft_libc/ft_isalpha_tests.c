d4962e484d2ce17a4dbd8ceff3c269fef161c11f
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-12-20 15:53:58 -0300

    Test the entire alphabet

--- tests/ft_libc/ft_isalpha_tests.c
+++ tests/ft_libc/ft_isalpha_tests.c
@@ -10,18 +10,20 @@ void run_ft_isalpha_tests() {
     when_ch_is_not_an_alphabetic_letter();
 }
 
-// to-do: use all aphabet letters
 static void when_ch_is_an_alphabetic_uppercase_letter() {
-    int result = ft_isalpha('A');
+    for (int i = 0; i < 26; i++) {
+        int result = ft_isalpha(UPPERCASE_ALPHABET[i]);
 
-    assert(result == 1);
+        assert(result == 1);
+    }
 }
 
-// to-do: use all aphabet letters
 static void when_ch_is_an_alphabetic_lowercase_letter() {
-    int result = ft_isalpha('g');
+    for (int i = 0; i < 26; i++) {
+        int result = ft_isalpha(LOWERCASE_ALPHABET[i]);
 
-    assert(result == 1);
+        assert(result == 1);
+    }
 }
 
 static void when_ch_is_not_an_alphabetic_letter() {
