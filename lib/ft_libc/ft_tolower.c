7c039b0b33286da3ef7488eba38630b2520c6716
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-12-20 16:22:19 -0300

    Implement ft_tolower

---
+++ lib/ft_libc/ft_tolower.c
@@ -0,0 +1,8 @@
+#include "ft_libc.h"
+
+int ft_tolower(int ch) {
+    int ch_position = 0;
+    for (ch_position; UPPERCASE_ALPHABET[ch_position] != '\0'; ch_position++)
+        if (UPPERCASE_ALPHABET[ch_position] == ch) return LOWERCASE_ALPHABET[ch_position];
+    return ch;
+}
