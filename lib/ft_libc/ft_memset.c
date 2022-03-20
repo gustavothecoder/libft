c975df5ac7c9b4b967620cb829041a670e1ebad1
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2022-03-20 15:29:15 -0300

    Remove unnecessary validation

--- lib/ft_libc/ft_memset.c
+++ lib/ft_libc/ft_memset.c
@@ -1,20 +1,10 @@
 #include "ft_libc.h"
 
-bool is_valid(void *dest, size_t count);
-
 void *ft_memset(void *dest, int ch, size_t count)
 {
-    if (is_valid(dest, count))
-    {
-        unsigned char converted_ch = (unsigned char)ch;
-        unsigned char *dest_pointer = (unsigned char *)dest;
-        for (int i = 0; i < count; i++)
-            dest_pointer[i] = ch;
-        return dest;
-    }
-}
-
-bool is_valid(void *dest, size_t count)
-{
-    return *(int *)dest != 0 && (count > 0 && count < SIZE_MAX);
+    unsigned char converted_ch = (unsigned char)ch;
+    unsigned char *dest_pointer = (unsigned char *)dest;
+    for (int i = 0; i < count; i++)
+        dest_pointer[i] = ch;
+    return dest;
 }
--- tests/ft_libc/ft_memset_tests.c
+++ tests/ft_libc/ft_memset_tests.c
@@ -1,14 +1,10 @@
 #include "tests.h"
 
 static void when_all_parameters_are_right();
-static void when_a_null_pointer_are_sent();
-static void when_count_is_greater_than_size_max();
 
 void run_ft_memset_tests()
 {
     when_all_parameters_are_right();
-    when_a_null_pointer_are_sent();
-    when_count_is_greater_than_size_max();
 }
 
 static void when_all_parameters_are_right()
@@ -20,24 +16,4 @@ static void when_all_parameters_are_right()
     assert_array(str, "aaaaahghghghghghghghgh", 22);
     assert_array(result, "aaaaahghghghghghghghgh", 22);
     assert(result == str);
-}
-
-static void when_a_null_pointer_are_sent()
-{
-    char *null_pointer = NULL;
-
-    char *result = (char *)ft_memset(&null_pointer, 'a', 5);
-
-    assert(null_pointer == NULL);
-    assert(result == NULL);
-}
-
-static void when_count_is_greater_than_size_max()
-{
-    char str[] = "ghghghghghghghghghghgh";
-
-    char *result = (char *)ft_memset(&str, 'a', SIZE_MAX + 1);
-
-    assert_array(str, "ghghghghghghghghghghgh", 22);
-    assert(result != str);
 }
